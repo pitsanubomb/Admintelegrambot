6e8c5a85ea7a4694910dd03d5b5cb21f1adb077c
cgloveut <pitsanubomb@gmail.com>
2021-09-07 13:43:50 +0700

    :fire: Fix bug deploy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const bot = app.get(getBotToken())
-  await app.listen(3000);
+  const bot = app.get(getBotToken());
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
