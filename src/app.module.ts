6ce0bebb2f88358965453f5555e9b8cc7a493279
cgloveut <pitsanubomb@gmail.com>
2021-09-09 18:28:10 +0700

    :fire: Test connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Telegraf } from './telegram/telegraf/telegraf.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [Telegraf],
+  imports: [
+    Telegraf,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ec2-54-147-126-173.compute-1.amazonaws.com',
+      port: 5432,
+      username: 'rjdhlfwaqxpncv',
+      password: '304aefa7acdaf02adc7fe306798538e5ee44d5d02387ed023dc66dc60e1b1d18',
+      database: 'dafsck06itqjrd',
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
