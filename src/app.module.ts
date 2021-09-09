626f62d9a8301b75b7b62b1eab259a3897e29088
cgloveut <pitsanubomb@gmail.com>
2021-09-09 18:47:10 +0700

    :fire: Fix bug :bug:

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,20 +5,7 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    Telegraf,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'ec2-54-147-126-173.compute-1.amazonaws.com',
-      port: 5432,
-      username: 'rjdhlfwaqxpncv',
-      password: '304aefa7acdaf02adc7fe306798538e5ee44d5d02387ed023dc66dc60e1b1d18',
-      database: 'dafsck06itqjrd',
-      ssl: false,
-      entities: ["dist/**/*.entity{.ts,.js}"],
-      synchronize: true,
-    }),
-  ],
+  imports: [Telegraf,TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
