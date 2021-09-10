ec96bfa710151ab23794ed54f87ddf2675e8c0a6
cgloveut <pitsanubomb@gmail.com>
2021-09-10 19:44:18 +0700

    :fire: Remove not use :open_umbrella:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Telegraf } from './telegram/telegraf/telegraf.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), Telegraf],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
