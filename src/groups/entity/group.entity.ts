26aadbfe187d712b5c8eada9b5ae1039e6b7c6e6
cgloveut <pitsanubomb@gmail.com>
2021-09-13 03:23:35 +0700

    :fire: Add group data structure :cloud_with_snow:

---
+++ src/groups/entity/group.entity.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Telegramgroup {
+    @PrimaryGeneratedColumn('uuid')
+    uid: string;
+
+    @Index()
+    @Column()
+    id:number;
+
+    @Index()
+    @Column()
+    groupname:string;
+}
