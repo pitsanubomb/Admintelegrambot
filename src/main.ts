76872b7f37e6ffddc0990a080c3ce8cb59966f46
cgloveut <pitsanubomb@gmail.com>
2021-09-14 16:14:16 +0700

    :fire: Add cors :sun_behind_rain_cloud:

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { getBotToken } from 'nestjs-telegraf';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const bot = app.get(getBotToken());
   await app.listen(process.env.PORT || 3000);
 }
